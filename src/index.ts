99208dc8a8b4dd97f70e3b71f624745c2e130fcc
betelgeussesoftware <betelgeussesoftware@gmail.com>
2024-09-07 13:00:07 -0600

    feat(reorder): stuff

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,70 @@
 import path from "path";
 import fs from "fs";
+import { compareHashes } from "jimp";
 
-function init() {
-  const readFrom: string = path.resolve("stickers");
-  const outDir: string = path.resolve("stickers-result");
+function logging(files: any[], percentageResult: number, i: number, j: number) {
+  console.log(`Comparing: i: ${files[i]} j:${files[j]}`);
+  console.log("------");
+  console.log(files[i]);
+  console.log(files[j]);
+  console.log(percentageResult);
+  console.log("------");
+}
 
-  let i = 0;
-  for (let file of fs.readdirSync(readFrom)) {
-    const pathFile = path.resolve(readFrom, file);
-    const stats = fs.statSync(path.resolve(readFrom, file));
-    const sizeByte = stats.size;
-    const fileName = `${sizeByte}_${Number(new Date())}.webp`;
-    fs.copyFileSync(pathFile, `${path.resolve(outDir, fileName)}`);
+async function init() {
+  const percentageAcceptance = 95;
 
-    console.log(`Saved ${i}`);
-    i++;
+  const files = fs.readdirSync(path.resolve("stickers"));
+  for (let i = 0; i < files.length - 1; i++) {
+    if (files[i]) {
+      fs.copyFileSync(
+        path.resolve("stickers", `${files[i]}`),
+        path.resolve("stickers-result", `${files[i]}`)
+      );
+    } else {
+      continue;
+    }
+
+    for (let j = 0; j < files.length - 1; j++) {
+      const fileFrom = files[i];
+      const fileAgainst = files[j];
+      const sameFile = files[i] === files[j];
+      if (!fileFrom || !fileAgainst || sameFile) {
+        continue;
+      }
+      const result = compareHashes(
+        path.resolve("stickers", fileFrom),
+        path.resolve("stickers", fileAgainst)
+      );
+
+      const percentageResult = (result - 1) * -1 * 100;
+      if (percentageResult > percentageAcceptance) {
+        logging(files, percentageResult, i, j);
+        const newFileName = `${files[i].replace(".webp", "")}_${Number(
+          new Date()
+        )}.webp`;
+        fs.copyFileSync(
+          path.resolve("stickers", fileAgainst),
+          `${path.resolve("stickers-result", newFileName)}`
+        );
+        files[j] = "";
+      }
+    }
   }
 }
 
+// function reorder() {
+//   const files = fs.readdirSync(path.resolve("stickers-result"));
+//   let i = 1;
+//   for (let file of files.sort()) {
+//     console.log(file, i);
+//     fs.copyFileSync(
+//       path.resolve("stickers-result", file),
+//       path.resolve("stickers-reordered", `${i}.webp`)
+//     );
+//     i++;
+//   }
+// }
+
 init();
+// reorder();
